1880e23a64cd443833de50849548671fe0fdcfb3
JuniorOsho <web2ajax@gmail.com>
2018-01-30 06:25:54 -0200

    audio

--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const multer = require('multer');
 const fs = require('fs');
+
 const getStat = require('util').promisify(fs.stat);
 
 //app.use(express.static('public'));
 // 10 * 1024 * 1024 // 10MB
 // usamos um buffer minúsculo! O padrão é 64k
 const highWaterMark =  2;
-
 const app = express();
+
+const DIR = './uploads/';
+const upload = multer({dest: DIR});
+/*
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});app.get('/api', function (req, res) {
+  res.end('file catcher example');
+});
+ 
+app.post('/api', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end(err.toString());
+    }
+ 
+    res.end('File is uploaded');
+  });
+});
+
+app.use(multer({
+  dest: DIR,
+  rename: function (fieldname, filename) {
+    return filename + Date.now();
+  },
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...');
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path);
+  }
+}));
+
+*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,10 +71,11 @@ app.get('/audio', async (req, res) => {
     const stream = fs.createReadStream(filePath, { highWaterMark });
 
     // só exibe quando terminar de enviar tudo
-    stream.on('end', () => console.log('acabou'));
+    stream.on('end', () => console.log('ACABOU DE TORAR'));
 
     // faz streaming do audio 
     stream.pipe(res);
 });
 
-app.listen(3000);
+//app.listen(3000);
+app.listen(3000, () => console.log('SERVIDOR NODE ATIVO NA 3000'));
